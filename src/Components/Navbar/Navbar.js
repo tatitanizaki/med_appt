b1858d42ad62148d77d73ac0cda276b38136161a
tatitanizaki <tatiana.tanizaki@gmail.com>
2024-04-03 22:23:20 -0400

    InstantConsultation is fully functional

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -43,6 +43,7 @@ const Navbar = () => {
                 <li className="link"><Link to="/search/doctors">Appointments</Link></li>
                 <li className="link"><Link to="/healthblog">Health Blog</Link></li>
                 <li className="link"><Link to="/reviews">Reviews</Link></li>
+                <li className="link"><Link to="/instant-consultation">Instant Consultation</Link></li>
                 
                 {isLoggedIn ? (
                     <>
