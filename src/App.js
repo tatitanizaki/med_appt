4b1f7de6097258d6689b997516257ee53aa95bd4
tatitanizaki <tatiana.tanizaki@gmail.com>
2024-04-03 22:41:09 -0400

    added Doc Search feature to Appointments

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LandingPage from './Components/LandingPage/LandingPage';
 import SignUp from './Components/Sign_Up/Sign_Up';
 import Login from './Components/Login/Login';
 import InstantConsultation from './Components/InstantConsultation/InstantConsultation'; 
+import FindDoctorSearch from './Components/FindDoctorSearch/FindDoctorSearch';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/signup" element={<SignUp/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/instant-consultation" element={<InstantConsultation/>} />
+        <Route path="/search/doctors" element={<FindDoctorSearch/>} />
       </Routes>
     </BrowserRouter>
   );
