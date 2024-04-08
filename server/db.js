5dc493e84d11c9e8ca4a100cf2dff37ca13f92c8
tatitanizaki <tatiana.tanizaki@gmail.com>
2024-04-08 15:50:56 -0400

    Added Review Forms

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:Mjg2MS10YXRpYW5h@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:MTM3NzgtdGF0aWFu@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
