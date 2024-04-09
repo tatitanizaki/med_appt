18a819d292a178a4ccbdd18afebcd31d400f593c
tatitanizaki <tatiana.tanizaki@gmail.com>
2024-04-09 11:58:05 -0400

    Deployed

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:MzA4MjgtdGF0aWFu@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:ODQ1LXRhdGlhbmF0@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
