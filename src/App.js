9a4d645ffafba71c719a92f52515da2023ec9a7a
tatitanizaki <tatiana.tanizaki@gmail.com>
2024-04-08 22:56:58 -0400

    Profile and Reports fully functional

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar';
 import LandingPage from './Components/LandingPage/LandingPage';
-import SignUp from './Components/Sign_Up/Sign_Up';
+import Sign_Up from './Components/Sign_Up/Sign_Up';
 import Login from './Components/Login/Login';
-import InstantConsultation from './Components/InstantConsultation/InstantConsultation'; 
+import InstantConsultation from './Components/InstantConsultation/InstantConsultation';
+import FindDoctorSearch from './Components/FindDoctorSearch/FindDoctorSearch';
 import BookingConsultation from './Components/BookingConsultation';
 import Notification from './Components/Notification/Notification';
-import FindDoctorSearch from './Components/FindDoctorSearch/FindDoctorSearch';
 import ReviewFrom from './Components/ReviewForm/ReviewForm';
+import ProfileForm from './Components/ProfileCard/ProfileCard';
+import ReportsLayout from './Components/ReportsLayout/ReportsLayout';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Notification>
-        <Routes>
-        <Route path="/" element={<LandingPage/>}/>
-        <Route path="/signup" element={<SignUp/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/instant-consultation" element={<InstantConsultation/>} />
-        <Route path='/finddoctor' element={<FindDoctorSearch />} />
-        <Route path='/search/doctors' element={<BookingConsultation />} />
-        <Route path='/reviews' element={<ReviewFrom />} />
-       {/* <Route path='/notification' element={<Notification />} /> */}
-      </Routes>
-      </Notification>
-    </BrowserRouter>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Notification>
+                    <Navbar />
+                    <Routes>
+                        <Route path="/LandingPage" element={<LandingPage />} />
+                        <Route path="/Sign_Up" element={<Sign_Up />} />
+                        <Route path="/Login" element={<Login />} />
+                        <Route path="/instant-consultation" element={<InstantConsultation />} />
+                        <Route path='/finddoctor' element={<FindDoctorSearch />} />
+                        <Route path='/search/doctors' element={<BookingConsultation />} />
+                        <Route path='/reviews' element={<ReviewFrom />} />
+                        <Route path='/profile' element={<ProfileForm />} />
+                        <Route path='/reports' element={<ReportsLayout />} />                    </Routes>
+                </Notification>
+            </BrowserRouter>
+        </div>
+    );
 }
-
 export default App;
-
-
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,66 +1,117 @@
 import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
     const [click, setClick] = useState(false);
     const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [userName, setUserName] = useState('');
-
-    // Hook for navigation
-    const navigate = useNavigate();
-
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [showDropdown, setShowDropdown] = useState(false);
     const handleClick = () => setClick(!click);
 
-    // Check login status on component mount
+    const handleLogout = () => {
+        sessionStorage.removeItem("auth-token");
+        sessionStorage.removeItem("name");
+        sessionStorage.removeItem("email");
+        sessionStorage.removeItem("phone");
+        // remove email phone
+        localStorage.removeItem("doctorData");
+        setIsLoggedIn(false);
+        // setUsername("");
+
+        // Remove the reviewFormData from local storage
+        for (let i = 0; i < localStorage.length; i++) {
+            const key = localStorage.key(i);
+            if (key.startsWith("reviewFormData_")) {
+                localStorage.removeItem(key);
+            }
+        }
+        setEmail('');
+        window.location.reload();
+    }
+    const handleDropdown = () => {
+        setShowDropdown(!showDropdown);
+    }
     useEffect(() => {
-        const token = sessionStorage.getItem('auth-token');
-        const name = sessionStorage.getItem('name'); // Assuming you store the user name in sessionStorage
-        if (token) {
+        const storedemail = sessionStorage.getItem("email");
+
+        if (storedemail) {
             setIsLoggedIn(true);
-            setUserName(name);
+            setUsername(storedemail);
         }
     }, []);
 
-    // Function to handle logout
-    const handleLogout = () => {
-        sessionStorage.clear(); // Clears the sessionStorage including the auth-token
-        setIsLoggedIn(false); // Update state to reflect logged-out status
-        navigate('/login'); // Redirect to the login page
-    };
+    useEffect(() => {
+        // Check if the user is already logged in
+        const storedUsername = sessionStorage.getItem("name");
 
+        if (storedUsername) {
+            setIsLoggedIn(true);
+            setUsername(storedUsername);
+        }
+    }, []);
     return (
         <nav>
             <div className="nav__logo">
-                <Link to="/">StayHealthy <i className="fa fa-user-md" style={{color:'#2190FF'}}></i></Link>
+                <Link to="/LandingPage">
+                    StayHealthy <i style={{ color: '#2190FF' }} className="fa fa-user-md"></i></Link>
+                <span>.</span>
             </div>
             <div className="nav__icon" onClick={handleClick}>
                 <i className={click ? "fa fa-times" : "fa fa-bars"}></i>
             </div>
-            
             <ul className={click ? 'nav__links active' : 'nav__links'}>
-                <li className="link"><Link to="/">Home</Link></li>
-                <li className="link"><Link to="/search/doctors">Appointments</Link></li>
-                <li className="link"><Link to="/healthblog">Health Blog</Link></li>
-                <li className="link"><Link to="/reviews">Reviews</Link></li>
-                <li className="link"><Link to="/instant-consultation">Instant Consultation</Link></li>
-                
+                <li className="link">
+                    <Link to="/LandingPage">Home</Link>
+                </li>
+                <li className="link">
+                    <Link to="/search/doctors">Appointments</Link>
+                </li>
+                <li className="link">
+                    <Link to="/healthblog">Health Blog</Link>
+                </li>
+                <li className="link">
+                    <Link to="/instant-consultation">Instant Consultation</Link>
+                </li>
+                <li className="link">
+                    <Link to="/reviews">Reviews</Link>
+                </li>
                 {isLoggedIn ? (
                     <>
-                        <li className="link" style={{ marginRight: '10px' }}>{userName}</li>
-                        <li className="link">
-                            <button onClick={handleLogout} className="btn1">Logout</button> {/* Use the same class as Sign Up/Login for consistency */}
+                        <li onClick={handleDropdown} className="link welcome-user">
+                            <p>
+                                Welcome, {username}
+                            </p>
+                            {showDropdown && (
+                                <ul className="dropdown-menu">
+                                    <li>
+                                        <Link to="/profile">Your Profile</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/reports">Your Reports</Link>
+                                    </li>
+                                </ul>
+                            )}
                         </li>
+                        <li className="link">
+                            <button className="btn2" onClick={handleLogout}>
+                                Logout
+                            </button>
+                        </li>
+
                     </>
                 ) : (
                     <>
                         <li className="link">
-                            {/* Wrap Link in button-like styles for consistency */}
-                            <Link to="/signup" className="btn1">Sign Up</Link>
+                            <Link to="/Sign_Up">
+                                <button className="btn1">Sign Up</button>
+                            </Link>
                         </li>
                         <li className="link">
-                            {/* Same as above */}
-                            <Link to="/login" className="btn1">Login</Link>
+                            <Link to="/Login">
+                                <button className="btn1">Login</button>
+                            </Link>
                         </li>
                     </>
                 )}
